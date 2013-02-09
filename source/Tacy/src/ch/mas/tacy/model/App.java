66aadcf7dc97fbdeeccd2fd2d925c9513d92de28
P. Büttiker <isnull@securityvision.ch>
2013-02-09 15:12:14 +0100

    added initial dummy agent code to Tacy

---
+++ source/Tacy/src/ch/mas/tacy/model/App.java
@@ -0,0 +1,13 @@
+package ch.mas.tacy.model;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("hello I am Tacy, your MAS!");
+	}
+
+}
